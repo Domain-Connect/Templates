baed7e57515bb5c3fac14bac028e187ea6ec0aa2
Sami Kerola <kerolasa@iki.fi>
2023-09-09 19:10:43 +0100

    improve domain connect template linter
    Convert string int's with warning to intergers. Catch duplicate providerId +
    serviceId fields when multiple templates are parsed. Ensure providerId and
    serviceId names are reasonable what comes to allowed characters. Check SRV
    protocol is one of the three possible usually used ones. Add additional
    record types to check list, and warn if unusual types are defined. Disallow
    record type being a domain connect variable.  And update go modules.
    
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- domain-connect-template-linter/domain-connect-template-linter.go
+++ domain-connect-template-linter/domain-connect-template-linter.go
@@ -16,6 +16,7 @@ import (
 	"io"
 	"os"
 	"path"
+	"strconv"
 	"strings"
 	"time"
 
@@ -36,12 +37,16 @@ const (
 	Max31b = 1<<31 - 1
 )
 
+var exitVal int
+var tlog zerolog.Logger
+var collision map[string]bool
+
 type Template struct {
 	ProviderID          string  `json:"providerId" validate:"required,min=1,max=64"`
 	ProviderName        string  `json:"providerName" validate:"required,min=1,max=64"`
 	ServiceID           string  `json:"serviceId" validate:"required,min=1,max=64"`
 	ServiceName         string  `json:"serviceName" validate:"required,min=1,max=255"`
-	Version             int     `json:"version,omitempty"`
+	Version             SINT    `json:"version,omitempty"`
 	Logo                string  `json:"logoUrl,omitempty", validate:"url"`
 	Description         string  `json:"description,omitempty"`
 	VariableDescription string  `json:"variableDescription,omitempty"`
@@ -63,20 +68,40 @@ type Record struct {
 	Type      string `json:"type" validate:"required,min=1,max=16"`
 	GroupID   string `json:"groupId,omitempty" validate:"min=1"`
 	Essential string `json:"essential,omitempty" validate:"oneof=Always OnApply"`
-	Host      string `json:"host" validate:"min=1,max=255"`
+	Host      string `json:"host,omitempty" validate:"min=1,max=255"`
 	Name      string `json:"name,omitempty" validate:"hostname"`
 	PointsTo  string `json:"pointsTo,omitempty" validate:"min=1,max=255"`
-	TTL       int    `json:"ttl" validate:"required,min=1"`
+	TTL       SINT   `json:"ttl" validate:"required,min=1"`
 	Data      string `json:"data,omitempty" validate:"min=1"`
 	TxtCMM    string `json:"txtConflictMatchingMode,omitempty" validate="oneof=None All Prefix"`
 	TxtCMP    string `json:"txtConflictMatchingPrefix,omitempty" validate:"min=1"`
-	Priority  int    `json:"priority,omitempty"`
-	Weight    int    `json:"weight,omitempty"`
-	Port      int    `json:"port,omitempty"`
+	Priority  SINT   `json:"priority,omitempty"`
+	Weight    SINT   `json:"weight,omitempty"`
+	Port      SINT   `json:"port,omitempty"`
 	Protocol  string `json:"protocol,omitempty" validate:"min=1"`
 	Service   string `json:"service,omitempty" validate:"min=1"`
 	Target    string `json:"target,omitempty" validate:"min=1"`
 	SPFRules  string `json:"spfRules,omitempty" validate:"min=1"`
+}
+
+type SINT int
+
+func (sint *SINT) UnmarshalJSON(b []byte) error {
+	if b[0] != '"' {
+		return json.Unmarshal(b, (*int)(sint))
+	}
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return err
+	}
+	tlog.Warn().Str("value", s).Msg("do not quote an integer, it makes it string")
+	exitVal = 1
+	*sint = SINT(i)
+	return nil
 }
 
 func setLoglevel(loglevel string) {
@@ -88,6 +113,11 @@ func setLoglevel(loglevel string) {
 }
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] <template.json> [...]\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "Warning. -inplace and -pretty will remove zero priority MX and SRV fields\n")
+	}
 	log.Logger = log.Output(
 		zerolog.ConsoleWriter{
 			Out:        os.Stderr,
@@ -103,28 +133,29 @@ func main() {
 	setLoglevel(*loglevel)
 
 	if flag.NArg() < 1 {
-		fmt.Fprintf(os.Stderr, "Usage: %s [options] <template.json> [template2.json ...]\n", os.Args[0])
-		flag.PrintDefaults()
-		log.Fatal().Msg("template file(s) as command argument is missing")
+		flag.Usage()
+		log.Fatal().Msg("template file(s) command argument is missing")
 	}
 
-	exitVal := 0
+	collision = make(map[string]bool)
+
 	for _, arg := range flag.Args() {
-		exitVal |= checkTemplate(arg)
+		checkTemplate(arg)
 	}
 	os.Exit(exitVal)
 }
 
-func checkTemplate(templatePath string) int {
-	retVal := 0
-	tlog := log.With().Str("template", templatePath).Logger()
+func checkTemplate(templatePath string) {
+	tlog = log.With().Str("template", templatePath).Logger()
 
 	f, err := os.Open(templatePath)
 	if err != nil {
 		tlog.Error().Err(err).Msg("cannot open file")
-		return 1
+		exitVal = 1
+		return
 	}
 	defer f.Close()
+	tlog.Debug().Msg("processing template")
 
 	var r io.Reader
 	r = f
@@ -137,15 +168,31 @@ func checkTemplate(templatePath string) int {
 
 	if err != nil {
 		tlog.Error().Err(err).Msg("json decode error")
-		retVal = 1
+		exitVal = 1
 	}
+
+	if checkInvalidChars(template.ProviderID) {
+		tlog.Error().Str("providerId", template.ProviderID).Msg("providerId contains invalid characters")
+		exitVal = 1
+	}
+
+	if checkInvalidChars(template.ServiceID) {
+		tlog.Error().Str("serviceId", template.ServiceID).Msg("serviceId contains invalid characters")
+		exitVal = 1
+	}
+
+	if _, found := collision[template.ProviderID+"/"+template.ServiceID]; found {
+		tlog.Error().Str("providerId", template.ProviderID).Str("serviceId", template.ServiceID).Msg("duplicate provierId + serviceId detected")
+		exitVal = 1
+	}
+	collision[template.ProviderID+"/"+template.ServiceID] = true
 
 	Validator := validator.New()
 	err = Validator.Struct(template)
 	if err != nil {
 		for _, err := range err.(validator.ValidationErrors) {
 			tlog.Warn().Err(err).Msg("template field validation")
-			retVal = 1
+			exitVal = 1
 		}
 	}
 
@@ -155,38 +202,47 @@ func checkTemplate(templatePath string) int {
 
 	if template.Shared {
 		tlog.Info().Msg("shared flag has been deprecated, use sharedProviderName instead")
-		// switch to false so that deprecated flag is not part of
-		// pretty printed output
+		// Override to ensure settings in pretty-print output are correct
 		template.Shared = false
+		template.SharedProviderName = true
 	}
 
 	if *Cloudflare {
-		retVal |= CloudflareTemplateChecks(tlog, template)
+		CloudflareTemplateChecks(template)
 	}
 
 	for rnum, record := range template.Records {
-		retVal |= checkRecord(tlog, rnum, record)
+		checkRecord(rnum, record)
 	}
 
 	if *PrettyPrint || *Inplace {
 		pp, err := json.Marshal(template)
 		if err != nil {
-			tlog.Error().Err(err).Msg("")
+			tlog.Error().Err(err).Msg("json marshaling failed")
 		}
 		var out bytes.Buffer
 		json.Indent(&out, pp, "", "    ")
 		fmt.Fprintf(&out, "\n")
 		if *Inplace {
-			WriteBack(tlog, templatePath, out)
+			writeBack(templatePath, out)
 		} else {
 			out.WriteTo(os.Stdout)
 		}
 	}
-
-	return retVal
 }
 
-func WriteBack(tlog zerolog.Logger, templatePath string, out bytes.Buffer) {
+const validChars = "-.0123456789_abcdefghijklmnopqrstuvwxy"
+
+func checkInvalidChars(s string) bool {
+	for _, char := range s {
+		if !strings.Contains(validChars, strings.ToLower(string(char))) {
+			return true
+		}
+	}
+	return false
+}
+
+func writeBack(templatePath string, out bytes.Buffer) {
 	f, err := os.CreateTemp("./", path.Base(templatePath))
 	if err != nil {
 		tlog.Warn().Err(err).Msg("could not create temporary file")
@@ -207,38 +263,29 @@ func WriteBack(tlog zerolog.Logger, templatePath string, out bytes.Buffer) {
 	tlog.Debug().Str("tmpfile", f.Name()).Msg("updated")
 }
 
-func checkRecord(tlog zerolog.Logger, rnum int, record Record) int {
+func checkRecord(rnum int, record Record) {
 	rlog := tlog.With().Int("record", rnum).Logger()
-	retVal := 0
 	switch record.Type {
 	case "CNAME", "NS":
 		if record.Host == "@" {
 			rlog.Error().Str("type", record.Type).Msg("record host must not be @")
-			retVal = 1
+			exitVal = 1
 		}
 		fallthrough
 	case "A", "AAAA":
 		if record.Host == "" {
 			rlog.Error().Str("type", record.Type).Msg("record host must not be empty")
-			retVal = 1
-		}
-		if record.TTL < 0 || Max31b < record.TTL {
-			rlog.Error().Str("type", record.Type).Int("ttl", record.TTL).Msg("invalid TTL")
-			retVal = 1
+			exitVal = 1
 		}
 
 	case "TXT":
 		if record.Host == "" {
 			rlog.Error().Str("type", record.Type).Msg("record host must not be empty")
-			retVal = 1
+			exitVal = 1
 		}
 		if record.Data == "" {
 			rlog.Error().Str("type", record.Type).Msg("record data must not be empty")
-			retVal = 1
-		}
-		if record.TTL < 0 || Max31b < record.TTL {
-			rlog.Error().Str("type", record.Type).Int("ttl", record.TTL).Msg("invalid TTL")
-			retVal = 1
+			exitVal = 1
 		}
 		if *Cloudflare {
 			if record.TxtCMM != "" || record.TxtCMM == "None" {
@@ -250,73 +297,96 @@ func checkRecord(tlog zerolog.Logger, rnum int, record Record) int {
 		} else {
 			if record.TxtCMM == "Prefix" && record.TxtCMP == "" {
 				rlog.Warn().Str("type", record.Type).Msg("record txtConflictMatchingPrefix is not defined")
-				retVal = 1
+				exitVal = 1
 			}
 		}
 
 	case "MX":
 		if record.Host == "" {
 			rlog.Error().Str("type", record.Type).Msg("record host must not be empty")
-			retVal = 1
+			exitVal = 1
 		}
 		if record.PointsTo == "" {
 			rlog.Error().Str("type", record.Type).Msg("record pointsTo must not be empty")
-			retVal = 1
-		}
-		if record.TTL < 0 || Max31b < record.TTL {
-			rlog.Error().Str("type", record.Type).Int("ttl", record.TTL).Msg("invalid TTL")
-			retVal = 1
+			exitVal = 1
 		}
 		if record.Priority < 0 || Max31b < record.Priority {
-			rlog.Error().Str("type", record.Type).Int("priority", record.Priority).Msg("invalid priority")
-			retVal = 1
+			rlog.Error().Str("type", record.Type).Int("priority", int(record.Priority)).Msg("invalid priority")
+			exitVal = 1
 		}
 
 	case "SRV":
 		if record.Name == "" {
 			rlog.Error().Str("type", record.Type).Msg("record name must not be empty")
-			retVal = 1
+			exitVal = 1
 		}
 		if record.Target == "" {
 			rlog.Error().Str("type", record.Type).Msg("record target must not be empty")
-			retVal = 1
+			exitVal = 1
+		}
+		if isInvalidProtocol(record.Protocol) {
+			rlog.Warn().Str("type", record.Type).Str("protocol", record.Protocol).Msg("invalid protocol")
+			exitVal = 1
 		}
 		if record.Priority < 0 || Max31b < record.Priority {
-			rlog.Error().Str("type", record.Type).Int("priority", record.Priority).Msg("invalid priority")
-			retVal = 1
+			rlog.Error().Str("type", record.Type).Int("priority", int(record.Priority)).Msg("invalid priority")
+			exitVal = 1
 		}
 		if record.Service == "" {
 			rlog.Error().Str("type", record.Type).Msg("record service must not be empty")
-			retVal = 1
+			exitVal = 1
 		}
 		if record.Weight < 0 || Max31b < record.Weight {
-			rlog.Error().Str("type", record.Type).Int("weigth", record.Weight).Msg("invalid weigth")
-			retVal = 1
+			rlog.Error().Str("type", record.Type).Int("weigth", int(record.Weight)).Msg("invalid weigth")
+			exitVal = 1
 		}
 		if record.Port < 1 || Max16b < record.Port {
-			rlog.Error().Str("type", record.Type).Int("port", record.Port).Msg("invalid port")
-			retVal = 1
+			rlog.Error().Str("type", record.Type).Int("port", int(record.Port)).Msg("invalid port")
+			exitVal = 1
 		}
 
 	case "SPFM":
 		if record.Host == "" {
 			rlog.Error().Str("type", record.Type).Msg("record host must not be empty")
-			retVal = 1
+			exitVal = 1
 		}
 		if record.SPFRules == "" {
 			rlog.Error().Str("type", record.Type).Msg("record spfRules must not be empty")
-			retVal = 1
+			exitVal = 1
 		}
+
+	case "APEXCNAME":
+		if *Cloudflare {
+			rlog.Info().Msg("Cloudflare does not support APEXCNAME, use CNAME instead")
+		}
+
+	case "REDIR301", "REDIR302":
+		if record.Target == "" {
+			rlog.Error().Str("type", record.Type).Msg("record target must not be empty")
+			exitVal = 1
+		}
+	default:
+		rlog.Info().Str("type", record.Type).Msg("unusual record type check DNS providers if they support it")
 	}
 
-	if strings.Count(record.GroupID, "%") > 1 {
+	if isVariable(record.Type) {
+		rlog.Error().Msg("record type must not be variable")
+		exitVal = 1
+	}
+
+	if record.TTL < 0 || Max31b < record.TTL {
+		rlog.Error().Str("type", record.Type).Int("ttl", int(record.TTL)).Msg("invalid TTL")
+		exitVal = 1
+	}
+
+	if isVariable(record.GroupID) {
 		rlog.Error().Msg("record groupId must not be variable")
-		retVal = 1
+		exitVal = 1
 	}
 
-	if strings.Count(record.TxtCMP, "%") > 1 {
+	if isVariable(record.TxtCMP) {
 		rlog.Error().Msg("record txtConflictMatchingPrefix must not be variable")
-		retVal = 1
+		exitVal = 1
 	}
 
 	if *Cloudflare {
@@ -324,19 +394,28 @@ func checkRecord(tlog zerolog.Logger, rnum int, record Record) int {
 			rlog.Info().Msg("Cloudflare does not support essential record settings")
 		}
 	}
-
-	return retVal
 }
 
-func CloudflareTemplateChecks(tlog zerolog.Logger, template Template) int {
-	retVal := 0
+func isInvalidProtocol(proto string) bool {
+	switch strings.ToLower(proto) {
+	case "_tcp", "_udp", "_tls":
+		return false
+	}
+	return true
+}
+
+func isVariable(s string) bool {
+	return strings.Count(s, "%") > 1
+}
+
+func CloudflareTemplateChecks(template Template) {
 	if template.SyncBlock {
 		tlog.Error().Msg("Cloudflare does not support syncBlock")
-		retVal = 1
+		exitVal = 1
 	}
 	if template.SyncPubKeyDomain == "" {
 		tlog.Error().Msg("Cloudflare requires syncPubKeyDomain")
-		retVal = 1
+		exitVal = 1
 	}
 	if template.SharedServiceName {
 		tlog.Info().Msg("Cloudflare does not support sharedServiceName")
@@ -353,5 +432,4 @@ func CloudflareTemplateChecks(tlog zerolog.Logger, template Template) int {
 	if template.HostRequired {
 		tlog.Info().Msg("Cloudflare does not support hostRequired")
 	}
-	return retVal
 }
--- domain-connect-template-linter/go.mod
+++ domain-connect-template-linter/go.mod
@@ -12,10 +12,10 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 )
